2c5602fcc0555517734f1f5a7d97524ba811f3c1
KUNAL SANJAY AGRAWAL <kunalagrawal96@gmail.com>
2016-09-26 20:53:13 +0530

    Add files via upload

---
+++ 1. Setup - Customer Analytics.R
@@ -0,0 +1,13 @@
+
+# Package installation
+## Description: Packages that ca
+install.packages("BTYD")
+install.packages("arules")
+install.packages("arulesViz")
+install.packages("ggplot2")
+
+# Package load
+library(BTYD)
+library(arules)
+library(arulesViz)
+library(ggplot2)
